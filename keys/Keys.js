f9dbc8b4a89264497ffc773b5431e6cf5e6fc6fe
ivsp <ivansperez93@gmail.com>
2023-07-19 10:11:10 +0200

    Remove old google api key

--- keys/Keys.js
+++ keys/Keys.js
@@ -1,11 +1,11 @@
-const googleKey = 'AIzaSyD_QXeJukwb3yHI4PqXoOZSkxPodSoFjbE'
-const attomo = ''
-const mapURL = `https://www.google.com/maps/embed/v1/place?key=${googleKey}`
+const googleKey = "f";
+const attomo = "";
+const mapURL = `https://www.google.com/maps/embed/v1/place?key=${googleKey}`;
 
 const key = {
-    googleKey,
-    attomo,
-    mapURL
-}
+  googleKey,
+  attomo,
+  mapURL,
+};
 
 export default key;
