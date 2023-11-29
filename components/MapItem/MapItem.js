290bdb7a1c0cbc24544b7e6a3a9cfcb7176aaeaa
Dayan Alvarez <dayan@attomo.digital>
2023-11-29 17:47:01 +0100

    added circle instead of marker in active map ubication

--- components/MapItem/MapItem.js
+++ components/MapItem/MapItem.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  LoadScript,
+  OverlayView,
+  OverlayViewF,
+} from "@react-google-maps/api";
 
-function MapItem({ lat, lng }) {
+function MapItem({ lat, lng, errorMargin }) {
   const coordinates = { lat: lat, lng: lng };
 
   const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_KEY;
+
+  const displacement = 0.002;
+
+  const displacedCoordinates = {
+    lat: coordinates.lat + displacement,
+    lng: coordinates.lng + displacement,
+  };
+
+  const getPixelPositionOffset = (width, height) => ({
+    x: -(width / 2),
+    y: -(height / 2),
+  });
 
   return (
     <>
@@ -15,7 +32,24 @@ function MapItem({ lat, lng }) {
             center={coordinates}
             zoom={15}
           >
-            <MarkerF position={coordinates} />
+            <OverlayViewF
+              position={displacedCoordinates}
+              mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+              getPixelPositionOffset={getPixelPositionOffset}
+            >
+              <div
+                style={{
+                  width: `${2 * errorMargin}px`,
+                  height: `${2 * errorMargin}px`,
+                  borderRadius: "50%",
+                  backgroundColor: "rgba(0, 0, 255, 0.2)",
+                  border: "1px solid blue",
+                  position: "absolute",
+                  transform: "translate(-50%, -50%)",
+                  pointerEvents: "none",
+                }}
+              />
+            </OverlayViewF>
           </GoogleMap>
         </LoadScript>
       )}
